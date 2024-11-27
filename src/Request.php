c12ccb9eb84f2ec5dabc1bcb7cd39bfd23ff20e9
Navid Torabazari <navid@Navids-MacBook-Pro.local>
2024-11-27 12:56:01 +0200

    Add same files with capital letter

---
+++ src/Request.php
@@ -0,0 +1,109 @@
+<?php 
+namespace Netopia\Payment2;
+
+class Request extends Start {
+    public $authenticationToken;
+    public $ntpID;
+    public $jsonRequest;
+
+    function __construct(){
+        parent::__construct();
+    }
+
+    public function setConfig($configData) {
+        $config = array(
+            'emailTemplate' => (string) isset($configData['emailTemplate']) ? $configData['emailTemplate'] : 'confirm',
+            'notifyUrl'     => (string) $configData['notifyUrl'],
+            'redirectUrl'   => (string) $configData['redirectUrl'],
+            'language'      => (string) isset($configData['language']) ? $configData['language'] : 'RO'
+        );
+        return $config;
+    }
+
+    public function setPayment($cardData, $threeDSecusreData) {
+        $threeDSecusreData = json_decode($threeDSecusreData);
+        $threeDSecusreData->IP_ADDRESS = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : "127.0.0.1";
+        
+        $payment = array(
+            'options' => [
+                'installments' => (int) 1,
+                'bonus'        => (int) 0
+            ],
+            'instrument' => [
+                'type'          => (string) "card",
+                'account'       => (string) $cardData['account'],
+                'expMonth'      => (int) $cardData['expMonth'],
+                'expYear'       => (int) $cardData['expYear'],
+                'secretCode'    => (string) $cardData['secretCode'],
+                'token'         => null
+            ],
+            'data' =>  $threeDSecusreData
+        );
+        return $payment;
+    }
+
+    /**
+     * Set the order
+     */
+    public function setOrder($orderData) {
+        $order = array(
+            'ntpID'         => (string) null, 
+            'posSignature'  => (string) $this->posSignature,
+            'dateTime'      => (string) date("c", strtotime(date("Y-m-d H:i:s"))),
+            'description'   => (string) $orderData->description,
+            'orderID'       => (string) $orderData->orderID,
+            'amount'        => (float)  $orderData->amount,
+            'currency'      => (string) $orderData->currency,
+            'billing'       => [
+                'email'         => (string) $orderData->billing->email,
+                'phone'         => (string) $orderData->billing->phone,
+                'firstName'     => (string) $orderData->billing->firstName,
+                'lastName'      => (string) $orderData->billing->lastName,
+                'city'          => (string) $orderData->billing->city,
+                'country'       => (int)    $orderData->billing->country,
+                'state'         => (string) $orderData->billing->state,
+                'postalCode'    => (string) $orderData->billing->postalCode,
+                'details'       => (string) $orderData->billing->details
+            ],
+            'shipping'      => [
+                'email'         => (string) $orderData->shipping->email,
+                'phone'         => (string) $orderData->shipping->phone,
+                'firstName'     => (string) $orderData->shipping->firstName,
+                'lastName'      => (String) $orderData->shipping->lastName,
+                'city'          => (string) $orderData->shipping->city,
+                'country'       => (int)    $orderData->shipping->country,
+                'state'         => (string) $orderData->shipping->state,
+                'postalCode'    => (string) $orderData->shipping->postalCode,
+                'details'       => (string) $orderData->shipping->details
+            ],
+            'products' => $orderData->products,
+            'installments'  => array(
+                                    'selected'  => (int) 1,
+                                    'available' => [(int) 0]
+                            ),
+            'data'       => null
+        );
+        return $order;
+    }
+
+
+    /**
+     * Set the request to payment
+     * @output json
+     */
+    public function setRequest($configData, $cardData, $orderData, $threeDSecusreData = null) {
+        $startArr = array(
+          'config'  => $this->setConfig($configData),
+          'payment' => $this->setPayment($cardData, $threeDSecusreData),
+          'order'   => $this->setOrder($orderData)
+      );
+      
+      // make json Data 
+      return json_encode($startArr);
+    }
+
+    public function startPayment(){
+      $result = $this->sendRequest($this->jsonRequest);
+      return($result);
+    }    
+}
---
+++ src/VerifyAuth.php
@@ -0,0 +1,110 @@
+<?php
+namespace Netopia\Payment2;
+
+class VerifyAuth extends Request {
+    // public $paRes;
+    public $postData;
+    public function __construct(){
+        parent::__construct();
+    }
+
+    public function setVerifyAuth() {
+        $paymentCartVerifyAuthParam = [
+            "authenticationToken" => (string) $this->authenticationToken,
+            "ntpID" => (string) $this->ntpID,
+            "formData" => $this->postData
+        ];
+
+        return (json_encode($paymentCartVerifyAuthParam));
+    }
+
+    // Send request to /payment/card/verify-auth
+    public function sendRequestVerifyAuth($jsonStr) {  
+        $url = $this->isLive ? 'https://secure.netopia-payments.com/api/payment/card/verify-auth' : 'https://secure-sandbox.netopia-payments.com/payment/card/verify-auth';
+        $ch = curl_init($url);
+    
+        $headers  = [
+            'Authorization: '.$this->apiKey,
+            'Content-Type: application/json'
+        ];
+
+        $payload = $jsonStr; // json DATA
+    
+        // Attach encoded JSON string to the POST fields
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+        
+        // Return response instead of outputting
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    
+
+        // Set the content type to application/json
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        // Execute the POST request
+        $result = curl_exec($ch);
+        
+        if (!curl_errno($ch)) {
+                switch ($http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE)) {
+                    case 200:  # OK  
+                        $arr = array(
+                            'status'  => 1,
+                            'code'    => $http_code,
+                            'message' => "Successfully verify authentication ",
+                            'data'    => json_decode($result)
+                        );
+                        break;
+                    case 404:  # Not Found
+                        $arr = array(
+                            'status'  => 0,
+                            'code'    => $http_code,
+                            'message' => "You send verify-auth request to wrong URL",
+                            'data'    => json_decode($result)
+                        );
+                        break;
+                    case 400:  # Bad Request
+                        $arr = array(
+                            'status'  => 0,
+                            'code'    => $http_code,
+                            'message' => "You send Bad Request to verify-auth",
+                            'data'    => json_decode($result)
+                        );
+                        break;
+                    case 401:  # Authorization required
+                        $arr = array(
+                            'status'  => 0,
+                            'code'    => $http_code,
+                            'message' => "You send without authorization",
+                            'data'    => json_decode($result)
+                        );
+                        break;
+                    case 405:  # Method Not Allowed
+                        $arr = array(
+                            'status'  => 0,
+                            'code'    => $http_code,
+                            'message' => "Your method of sending data to verify-auth are Not Allowed",
+                            'data'    => json_decode($result)
+                        );
+                        break;
+                    default:
+                        $arr = array(
+                            'status'  => 0,
+                            'code'    => $http_code,
+                            'message' => "Opps! Something is wrong, verify how you send data to verify-auth & try again!!!",
+                            'data'    => json_decode($result)
+                        );
+                }
+            } else {
+                $arr = array(
+                    'status'  => 0,
+                    'code'    => 0,
+                    'message' => "Opps! There is some problem, you are not able to send data to verify-auth!!!"
+                );
+            }
+        
+        // Close cURL resource
+        curl_close($ch);
+        
+        $finalResult = json_encode($arr, JSON_FORCE_OBJECT);
+        return $finalResult;
+    }
+}
